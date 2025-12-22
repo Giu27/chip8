b1630a0736b7dda8b8dc88d104460d6d8cf8abc2
Giu27 <Caruso.giuseppe09@hotmail.com>
2025-12-22 21:47:06 +0100

    rom loading

--- include/chip8.h
+++ include/chip8.h
@@ -1,2 +1,21 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
+class Chip8{
+    private:
+        uint8_t RAM[4096]; //4kB of RAM
+        uint16_t PC, I; //16 bits Program Counter and Index Register
+        uint8_t V[16]; //Vx Registers, V0 through VF
+
+        uint16_t stack[16]; //Stack
+        uint8_t sp; //stack pointer
+
+        uint8_t delay_timer, sound_timer; //timers
+
+        bool display[64 * 32]; //display
+    
+    public:
+        Chip8();
+        void load_rom(std::string path);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,8 @@
 #include <chip8.h>
+#include <SDL3\SDL.h>
+#include <iostream>
+
+int main(){
+    Chip8 chip8;
+    chip8.load_rom("roms/IBM Logo.ch8");
+}
